7f762c73a5c900d8500dc012eb46a2c0127a8585
Barath Kumar <barath.kumar@daato.net>
2023-03-25 20:13:08 +0100

    feat: maas

--- setup-ubuntu/maas.sh
+++ setup-ubuntu/maas.sh
@@ -7,3 +7,19 @@ sudo cat maas-baremetal-k8s-tutorial/lxd.conf | lxd init --preseed
 lxc network show lxdbr0
 lxd waitready
 sudo maas init region+rack --database-uri maas-test-db:/// --maas-url http://${IP_ADDRESS}:5240/MAAS
+sleep 30
+sudo maas createadmin --username admin --password admin --email admin
+export APIKEY=$(sudo maas apikey --username admin)
+maas login admin 'http://localhost:5240/MAAS/' $APIKEY
+
+export SUBNET=10.10.10.0/24
+export FABRIC_ID=$(maas admin subnet read "$SUBNET" | jq -r ".vlan.fabric_id")
+export VLAN_TAG=$(maas admin subnet read "$SUBNET" | jq -r ".vlan.vid")
+export PRIMARY_RACK=$(maas admin rack-controllers read | jq -r ".[] | .system_id")
+maas admin subnet update $SUBNET gateway_ip=10.10.10.1
+maas admin ipranges create type=dynamic start_ip=10.10.10.200 end_ip=10.10.10.254
+maas admin vlan update $FABRIC_ID $VLAN_TAG dhcp_on=True primary_rack=$PRIMARY_RACK
+maas admin maas set-config name=upstream_dns value=8.8.8.8
+export VM_HOST_ID=$(maas admin vm-hosts create  password=password  type=lxd power_address=https://${IP_ADDRESS}:8443 project=maas | jq '.id')
+maas admin vm-host update $VM_HOST_ID cpu_over_commit_ratio=4
+maas admin tags create name=metal comment='This tag should to machines that will be used as bare metal'
